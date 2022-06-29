01c1bea2032db14f6752971512595f7b1e0f2a7e
Kevin C. Zimmerman <kevinczimmerman@gmail.com>
2022-06-28 21:11:33 -0500

    day 3 part 2

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -1,6 +1,9 @@
-use advent_of_code_2016::day3::{num_possible_triangles, INPUT};
+use advent_of_code_2016::day3::{num_possible_triangles, num_possible_vertical_triangles, INPUT};
 
 fn main() {
     let num_possible = num_possible_triangles(INPUT);
     println!("part1: {num_possible}");
+
+    let num_possible = num_possible_vertical_triangles(INPUT);
+    println!("part2: {num_possible}");
 }
--- src/day3/mod.rs
+++ src/day3/mod.rs
@@ -1,5 +1,6 @@
 pub const INPUT: &str = include_str!("input.txt");
 
+#[derive(Debug)]
 pub struct Triangle(usize, usize, usize);
 
 impl Triangle {
@@ -19,6 +20,41 @@ impl From<&str> for Triangle {
     }
 }
 
+fn into_vertical_triangles(input: &str) -> Vec<Triangle> {
+    let numbers: Vec<usize> = input
+        .split_whitespace()
+        .map(|part| part.parse().unwrap())
+        .collect();
+
+    let mut triangles = Vec::with_capacity(numbers.len() / 3);
+
+    let mut i = 0;
+    let mut count = 0;
+    for _ in 0..triangles.capacity() {
+        let a = numbers[i];
+        let b = numbers[i + 3];
+        let c = numbers[i + 6];
+        triangles.push(Triangle(a, b, c));
+
+        count += 1;
+        if count == 3 {
+            count = 0;
+            i += 7;
+        } else {
+            i += 1;
+        }
+    }
+
+    triangles
+}
+
+pub fn num_possible_vertical_triangles(input: &str) -> usize {
+    into_vertical_triangles(input)
+        .iter()
+        .filter(|&t| t.is_possible())
+        .count()
+}
+
 pub fn num_possible_triangles(input: &str) -> usize {
     input
         .lines()
@@ -33,5 +69,20 @@ mod tests {
     #[test]
     fn bad_triangle() {
         assert!(!Triangle::from("5 10 25").is_possible());
+    }
+
+    #[test]
+    fn vertical_triangles() {
+        assert_eq!(
+            num_possible_vertical_triangles(
+                "101 301 501
+102 302 502
+103 303 503
+201 401 601
+202 402 602
+203 403 603",
+            ),
+            6
+        );
     }
 }
